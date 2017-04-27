278cf8f1c589b22288febc2542e4c6b2c929ce10
Tor Robinson <torrobinson@gmail.com>
2017-04-27 13:19:53 -0600

    Reimplemented a custom hypoentuse solver, as the built-in one is not supported in some versions of IE yet

--- js/game/Point.js
+++ js/game/Point.js
@@ -5,6 +5,6 @@ class Point{
   }
 
   static getDistanceBetweenPoints(point1,point2){
-    return Math.hypot(point1.x-point2.x, point1.y-point2.y);
+    return Math.sqrt((point1.x-point2.x)*(point1.x-point2.x) + (point1.y-point2.y)*(point1.y-point2.y));
   }
 }
