e7410fc8998a4a72795d39814d5acd86a512656b
Tor Robinson <torrobinson@gmail.com>
2017-04-27 20:51:40 -0600

    Actor now has a facing direction TextRenderer temporarily updated to draw player direction Fixes #6

--- js/game/Actors/Base/Actor.js
+++ js/game/Actors/Base/Actor.js
@@ -3,12 +3,15 @@ class Actor{
     // Actors must be born with awareness of the game they are in
     this.game = game;
 
+    this.facing = Directions.Down;
+
     this.character = ' ';
     this.location = null;
     this.layer = null;
   }
 
   move(direction){
+    this.facing = direction;
     var offsetToMove = Movement.DirectionToOffset(direction);
     if(this.layer !== null){
       var moveTo = Movement.AddPoints(this.location, offsetToMove);
@@ -26,15 +29,18 @@ class Actor{
     }
   }
 
+  // Generic action to perform when any collision happened
   collided(){
 
   }
 
+  // When tried to move into another object
   collidedInto(actor){
     this.collided();
 
   }
 
+  // When another object tried to move into me
   collidedBy(actor){
     this.collided();
 
