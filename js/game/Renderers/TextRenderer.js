87fdfc548dd15a297bf57d5f4d23418942c0520a
Tor Robinson <torrobinson@gmail.com>
2017-04-29 09:42:46 -0600

    Increased font-size of TextRenderer

--- js/game/Renderers/TextRenderer.js
+++ js/game/Renderers/TextRenderer.js
@@ -41,6 +41,7 @@ class TextRenderer extends Renderer{
           textArea.style.top = 0;
           textArea.style.zIndex = layer.zIndex;
           textArea.style.overflowY = 'hidden';
+          textArea.style.fontSize = '20px';
 
           // Draw each layer from smallest to highest index
           var text = '';
