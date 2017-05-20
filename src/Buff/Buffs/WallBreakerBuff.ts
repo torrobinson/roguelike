38e585b4c34aecd7ae66452a6a85c3bdd1617f87
Tor Robinson <torrobinson@gmail.com>
2017-05-20 10:20:53 -0600

    Fix to WallBreaker - Don't allow player to break walls that border the map

--- src/Buff/Buffs/WallBreakerBuff.ts
+++ src/Buff/Buffs/WallBreakerBuff.ts
@@ -6,11 +6,21 @@ class WallBreakerBuff extends Buff {
         return "Destroy the next " + this.getUsesRemaining() + ' walls you touch';
     }
 
-    // The wall breaker buff causes things the actor bumps into to be destroyed immediately
+    // The wall breaker buff causes walls the actor bumps into to be destroyed immediately
     onCollideBefore(bumped: Actor) {
+        // If we hit a wall
         if (bumped instanceof Wall) {
-            bumped.die();
-            this.used();
+            // And the wall isn't the border of the map
+            //   (we dont want the player to try leave the map)
+            if (
+                bumped.location.x !== 0 &&
+                bumped.location.x !== bumped.layer.width - 1 &&
+                bumped.location.y !== 0 &&
+                bumped.location.y !== bumped.layer.height - 1
+            ) {
+                bumped.die();
+                this.used();
+            }
         }
     }
 }
